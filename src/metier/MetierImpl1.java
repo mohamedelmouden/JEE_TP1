af727c164be45a749b840ccf4e7fcdf3269d8947
mohamedelmouden <m.elmouden1908@uca.ac.ma>
2026-03-10 00:41:06 +0000

    Refactor MetierImpl1 by removing comments
    Removed unnecessary comments and cleaned up code formatting.

--- src/metier/MetierImpl1.java
+++ src/metier/MetierImpl1.java
@@ -1,16 +1,16 @@
 package metier;
-
+// message personnelle::0637
 import dao.IDao;
-
+// message personnelle::0637
 public class MetierImpl1 implements IMetier {
     private IDao dao;
-
+// message personnelle::0637
     public void setDao(IDao dao) {
         this.dao = dao;
-    }
+    }// message personnelle::0637
 
     @Override
     public double calcul() {
-        return dao.getValue() * 42;
+        return dao.getValue() * 42;// message personnelle::0637
     }
 }
