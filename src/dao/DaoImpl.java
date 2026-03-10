124abcbb050e3cb53b566595c3d8f853811099df
mohamedelmouden <m.elmouden1908@uca.ac.ma>
2026-03-10 00:39:28 +0000

    Annotate getValue method with personal message
    Added personal message annotation to getValue method.

--- src/dao/DaoImpl.java
+++ src/dao/DaoImpl.java
@@ -1,8 +1,9 @@
 package dao;
-
+// message personnelle::0637
 public class DaoImpl implements IDao {
-    @Override
-    public double getValue() {
+    @Override  
+    // message personnelle::0637
+    public double getValue() {// message personnelle::0637
         return 100.0;
-    }
+    }// message personnelle::0637
 }
